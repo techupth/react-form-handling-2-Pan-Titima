10ade6301dee30d4faedd1bc50117d7705de8c90
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-08-19 23:41:54 +0700

    Answer Exercise

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 function ProductForm() {
+
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState(0.0);
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = () => {
+    const data = {
+      name : name,
+      price : price,
+      image : image,
+      description : description
+    }
+
+    window.alert(JSON.stringify(data));
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +29,7 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(event) => {setName(event.target.value)}}
           />
         </label>
       </div>
@@ -22,7 +41,7 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(event) => {setImage(event.target.value)}}
           />
         </label>
       </div>
@@ -34,7 +53,7 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(event) => {setPrice(event.target.value)}}
           />
         </label>
       </div>
@@ -46,7 +65,7 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(event) => {setDescription(event.target.value)}}
             rows={4}
             cols={30}
           />
